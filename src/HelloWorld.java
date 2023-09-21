39d837bcfe17de41517b0cfa9caed66110eeadf8
eric.cristelli <eric.cristelli@buonarroti.tn.it>
2023-09-21 14:10:18 +0200

    feat: classe che gestisce il saluto.

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    private String saluto;
+
+    public HelloWorld(){
+        saluto = "ciao";
+    }
+
+    public String toString(){
+        return saluto;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HelloWorld hello1 = new HelloWorld();
+        System.out.println(hello1.toString());
     }
 }
